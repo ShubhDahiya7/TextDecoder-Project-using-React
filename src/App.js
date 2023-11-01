981d309ff32e07ce7b9170da36d8696252722f15
ShubhDahiya7 <pespro2727@gmail.com>
2023-11-01 08:54:34 +0530

    initial_commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Navbar from "./components/Navbar";
+
+
+// lets declare global js variable
+//let name = "shubham"
+function App() {
+  // Important pts -
+  // everything inside the return of this function based component is jsx
+  // app.js file is what finally runs on our react-application
+  // in jsx we use className instead of class we write in html
+  // camelCasing is used while writing code
+  // Everything inside {} curvy brackets is seen/resolved as js inside function component
+  return (
+      <>
+          <Navbar title="TextI" about="Aboutus"/>
+      </>
+  );
+}
+
+export default App;
